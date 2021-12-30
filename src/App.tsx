6d2a551dc4ac0e716d047acf15c46e173fb07202
Oskar Jiang <oskar.jiang@polestar.com>
2021-12-30 20:28:06 +0100

    Add interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Typography,
+} from "@mui/material";
+import { IPlayer } from "./domain/player";
 
 function App() {
+  const [players, setPlayers] = useState<IPlayer[]>([]);
+  const [open, setOpen] = useState(false);
+  const [player, setPlayer] = useState<IPlayer>({ name: "" });
+
+  const getPlayers = async () => {
+    const response = await fetch(
+      "https://gameikvall.logotype.workers.dev/players"
+    );
+    const data = await response.json();
+    setPlayers(data);
+  };
+  const addPlayer = async (player: IPlayer) => {
+    const response = await fetch(
+      "https://gameikvall.logotype.workers.dev/addPlayer",
+      {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(player),
+      }
+    );
+    const data = await response.json();
+    setPlayers(data);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  useEffect(() => {
+    getPlayers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card sx={{ width: 360 }}>
+        <CardContent>
+          <Typography component="div" variant="h4">
+            Game ikväll?
+          </Typography>
+          <List
+            sx={{ width: "50%", maxWidth: 180, bgcolor: "background.paper" }}
+          >
+            {players.map((player) => (
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar></Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={player.name} />
+              </ListItem>
+            ))}
+          </List>
+          <Button variant="contained" onClick={handleClickOpen}>
+            Game ikväll
+          </Button>
+        </CardContent>
+      </Card>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Namn"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setPlayer({ ...player, name: e.target.value })}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Avbryt</Button>
+          <Button
+            onClick={() => {
+              addPlayer(player);
+              getPlayers();
+              handleClose();
+            }}
+          >
+            Jag är på
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
