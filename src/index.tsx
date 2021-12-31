10e7fbbe3094c44c421184f01903758c3baee855
Oskar Jiang <oskar.jiang@polestar.com>
2021-12-31 15:19:14 +0100

    Add cookie banner

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CookieConsent from "react-cookie-consent";
 
 ReactDOM.render(
   <React.StrictMode>
+    <CookieConsent>
+      This website uses cookies to enhance the user experience.
+    </CookieConsent>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
