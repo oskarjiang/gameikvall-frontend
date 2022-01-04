1d30fbe9300e2a48172edde5f5cb0bec44bca629
Oskar Jiang <oskar.jiang@polestar.com>
2022-01-04 20:25:17 +0100

    Add expirydate for cookie

--- src/App.tsx
+++ src/App.tsx
@@ -136,7 +136,12 @@ function App() {
               if (player) addPlayer(player!);
               getPlayers();
               if (remember) {
-                document.cookie = `player=${JSON.stringify(player)}`;
+                const d = new Date();
+                d.setTime(d.getTime() + 365 * 24 * 60 * 60 * 1000);
+                const expires = "expires=" + d.toUTCString();
+                document.cookie = `player=${JSON.stringify(
+                  player
+                )}; ${expires};`;
               }
               handleClose();
             }}
